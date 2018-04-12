f2942191cb6d9015b59be2953e4c460b6112643c
Raphael Emberger <raember@users.noreply.github.com>
2018-04-12 15:21:54 +0000

    Fixed color script

--- color_script.sh
+++ color_script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo -e "\033[2mNORMAL:"
+echo -e "\033[mNORMAL:"
 echo -e "\033[30mBLACK \t\033[90mLIGHT_BLACK"
 echo -e "\033[31mBLUE  \t\033[91mLIGHT_BLUE"
 echo -e "\033[32mGREEN \t\033[92mLIGHT_GREEN"
@@ -9,7 +9,7 @@ echo -e "\033[34mRED   \t\033[94mLIGHT_RED"
 echo -e "\033[35mPURPLE\t\033[95mLIGHT_PURPLE"
 echo -e "\033[36mYELLOW\t\033[96mLIGHT_YELLOW"
 echo -e "\033[37mGRAY  \t\033[97mLIGHT_GRAY"
-echo -e "\033[1mBOLD:"
+echo -e "\033[mBOLD:"
 echo -e "\033[30mBLACK \t\033[90mLIGHT_BLACK"
 echo -e "\033[31mBLUE  \t\033[91mLIGHT_BLUE"
 echo -e "\033[32mGREEN \t\033[92mLIGHT_GREEN"
