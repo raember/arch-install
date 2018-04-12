d3e21b0ee24f4b5a46e2b1931f699b536cea363c
Raphael Emberger <emberrap@students.zhaw.ch>
2018-04-12 17:57:58 +0200

    Finished color script

--- color_script.sh
+++ color_script.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 echo -e "    N    L  "
-echo -e "0 \033[40;37m   BLACK    \033[100;37m   DARK GRAY   \033[m"
-echo -e "1 \033[41;37m    RED     \033[101;37m   LIGHT RED   \033[m"
-echo -e "2 \033[42;37m   GREEN    \033[102;37m  LIGHT GREEN  \033[m"
-echo -e "3 \033[43;30m   YELLOW   \033[103;30m LIGHT YELLOW  \033[m"
-echo -e "4 \033[44;37m    BLUE    \033[104;37m  LIGHT BLUE   \033[m"
-echo -e "5 \033[45;37m  MAGENTA   \033[105;37m LIGHT MAGENTA \033[m"
-echo -e "6 \033[46;37m    CYAN    \033[106;37m  LIGHT CYAN   \033[m"
+echo -e "0 \033[40;39m   BLACK    \033[100;39m   DARK GRAY   \033[m"
+echo -e "1 \033[41;39m    RED     \033[101;30m   LIGHT RED   \033[m"
+echo -e "2 \033[42;39m   GREEN    \033[102;30m  LIGHT GREEN  \033[m"
+echo -e "3 \033[43;39m   YELLOW   \033[103;30m LIGHT YELLOW  \033[m"
+echo -e "4 \033[44;39m    BLUE    \033[104;30m  LIGHT BLUE   \033[m"
+echo -e "5 \033[45;39m  MAGENTA   \033[105;30m LIGHT MAGENTA \033[m"
+echo -e "6 \033[46;39m    CYAN    \033[106;30m  LIGHT CYAN   \033[m"
 echo -e "7 \033[47;30m LIGHT GRAY \033[107;30m     WHITE     \033[m"
